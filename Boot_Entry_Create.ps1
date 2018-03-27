b855832e933d2b5a6aa85a110a87d23dc85546bd
Davis Lin <dlin@DA-MBP13.local>
2018-03-28 01:03:15 +0800

    Fix code

--- Boot_Entry_Create.ps1
+++ Boot_Entry_Create.ps1
@@ -5,23 +5,26 @@
 $Directory_BootScript = "D:\boot_script"
 $Directory_BootEntry = "D:\boot_entry"
 
+# The empty VHDX file for cloning to any other boot entries
+$File_VHDTemplate = "0_empty.vhdx"
+
+# The player list file, the format should be CSV.
+$File_PlayerListCSV = "player_list.csv"
+
+# The output json file for player list.
+$File_PlayerListJSON = "player_list.json"
+
 # The drive letter of where VHDX files exist
 $Drive_VHD = $Directory_BootEntry.Split(':')[0]
 # The directory of where VHDX files exist. (Not includ drive letter, only directory structure.)
 $SubPath_VHD = $Directory_BootEntry.Split('\')[1..10] -join '\'
 
-# The empty VHDX file for cloning to any other boot entries
-$File_VHDTemplate = "0_empty.vhdx"
 # The full path of the empty VHD file.
 $FullPath_VHDTemplate = "$Directory_BootEntry\$File_VHDTemplate"
 
-# The player list file, the format should be CSV.
-$File_PlayerListCSV = "player_list.csv"
 # The full path of the player list file.
 $FullPath_PlayerListCSV = "$Directory_BootScript\$File_PlayerListCSV"
 
-# The output json file for player list.
-$File_PlayerListJSON = "player_list.json"
 # The full path of the output json file.
 $FullPath_PlayerListJSON = "$Directory_BootScript\$File_PlayerListJSON"
 
@@ -49,7 +52,7 @@ if ([System.IO.File]::Exists($FullPath_PlayerListJSON)) {
 # Convert the input CSV file to JSON file.
 Get-Content -Path $FullPath_PlayerListCSV | ConvertFrom-Csv -Delimiter ',' | ConvertTo-Json | Out-File $FullPath_PlayerListJSON
 
-# Get the last boot entry GUID
+
 # Add new boot entry
 Write-Host "Create a temporary boot entry"
 $New_Boot_Entry = cmd /c "bcdedit /copy {current} /d TemporaryBootEntry"
@@ -61,17 +64,22 @@ Write-Host "`nThe last used GUID is $GUID"
 # Delete the temporary boot entry
 Write-Host "`nDelete the temporary boot entry"
 cmd /c "bcdedit /delete $GUID"
-
 Write-Host "`nDelete GUID $GUID"
 
+# Parse the last GUID
 $GUID = ($GUID.Split('{')[1]).split('}')[0]
 $GUID_1st = $GUID.Split('-')[0]
 $GUID_2st = $GUID.Split('-')[1..4] -join '-'
+# Convert the GUID string to hex code
 $GUID_Current = [System.Convert]::ToString("0x$GUID_1st", 10)
 
+# Save the boot menu to $BCD variable
 $BCD = bcdedit
 
+# Read the JSON file and convert to object format
 $JSONContent = Get-Content -Path $FullPath_PlayerListJSON | ConvertFrom-Json
+
+# Iterate each object
 $JSONContent | ForEach-Object {
     $Team = $_."Team"
     $BlizzardID = $_."BlizzardID"
@@ -79,6 +87,7 @@ $JSONContent | ForEach-Object {
     $File_NewVHD = "$NewBootEntry.vhdx"
     $FullPath_NewVHD = "$Directory_BootEntry\$File_NewVHD"
     
+    # Generate player's VHD file from template file if it doesn't exist.
     Write-Host "`n$SequentialNumber $Team $BlizzardID"
     if ([System.IO.File]::Exists($FullPath_NewVHD)) {
         Write-Host "The VHD file $File_NewVHD exists!"
@@ -88,6 +97,7 @@ $JSONContent | ForEach-Object {
         Write-Host "Copy $File_VHDTemplate to $File_NewVHD"
     }    
     
+    # Generate boot entry if it doesn't exist.
     if ($BCD | findstr "description" | findstr "$NewBootEntry") {
         Write-Host "The boot entry ${Team}_${BlizzardID} exists!"
     }
